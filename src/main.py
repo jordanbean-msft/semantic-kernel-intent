a3d360857f3c857e370625c09ddf49875e53e83c
Jordan Bean <84806584+jordanbean-msft@users.noreply.github.com>
2024-02-22 14:29:31 -0600

    Dev (#1)
    * init
    
    * intent
    
    * working search
    
    * refactor
    
    * new files
    
    * reorg

---
+++ src/main.py
@@ -0,0 +1,157 @@
+import asyncio
+import os
+
+import semantic_kernel as sk
+import semantic_kernel.connectors.ai.open_ai as sk_oai
+from semantic_kernel.connectors.ai.open_ai.request_settings.azure_chat_request_settings import (
+    AzureAISearchDataSources,
+    AzureChatRequestSettings,
+    AzureDataSources,
+    ExtraBody,
+)
+from semantic_kernel.connectors.ai.open_ai.semantic_functions.open_ai_chat_prompt_template import (
+    OpenAIChatPromptTemplate,
+)
+
+plugins_directory = os.path.join(__file__, "../plugins")
+kernel = sk.Kernel()
+deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env() # type: ignore
+
+def generate_initial_chat_function(kernel) -> sk.KernelFunctionBase:
+    system_message = """
+    ChatBot can have a conversation with you about any topic.
+    It can give explicit instructions or say 'I don't know'
+    when it doesn't know the answer.
+    """
+
+    req_settings = AzureChatRequestSettings(
+        service_id="chat-gpt",
+        ai_model_id=deployment,
+        max_tokens=2000,
+        temperature=0.7,
+        top_p=0.8,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        number_of_responses=1
+    )
+    prompt_config = sk.PromptTemplateConfig(execution_settings=req_settings)
+
+    chat_service = sk_oai.AzureChatCompletion(
+        deployment_name=deployment,
+        api_key=api_key,
+        endpoint=endpoint,
+        api_version="2023-12-01-preview",
+        use_extensions=False,
+    )
+
+    kernel.add_chat_service("chat-gpt", chat_service)
+
+    prompt_template = OpenAIChatPromptTemplate(
+    template="{{$user_input}}", 
+    template_engine=kernel.prompt_template_engine, 
+    prompt_config=prompt_config)
+
+    prompt_template.add_system_message(system_message)
+
+    function_config = sk.SemanticFunctionConfig(prompt_config, prompt_template)
+    chat_function = kernel.register_semantic_function("ChatBot", "Chat", function_config)
+    return chat_function
+
+chat_function = generate_initial_chat_function(kernel)
+
+intent_plugin = kernel.import_semantic_plugin_from_directory(plugins_directory + "/semantic", "IntentDetectionPlugin")
+
+def setup_kernel_for_specific_index(context: sk.KernelContext, kernel: sk.Kernel) -> sk.KernelFunctionBase:
+    if(context.result == "movies"):    
+        chat_function = generate_new_chat_function_using_index(kernel=kernel, index_name="movies")
+        return chat_function
+    
+    if(context.result == "songs"):
+        chat_function = generate_new_chat_function_using_index(kernel=kernel, index_name="songs")
+        return chat_function
+    
+    raise Exception(f"No function found: {context.result}")
+
+def generate_new_chat_function_using_index(kernel: sk.Kernel, index_name: str) -> sk.KernelFunctionBase:
+    search_api_key, search_url = sk.azure_aisearch_settings_from_dot_env(include_index_name=False) # type: ignore
+    azure_ai_search_settings = {
+            "key": search_api_key, 
+            "endpoint": search_url, 
+            "indexName": index_name, # this is the name of the specific Azure AI Search index you want to use
+            "fieldsMapping": {
+                "titleField": "title",
+                "contentFields": ["content"],
+                "vectorFields": ["contentVector"]
+            }
+        }
+        
+    az_source = AzureAISearchDataSources(**azure_ai_search_settings) # type: ignore
+    az_data = AzureDataSources(type="AzureCognitiveSearch", parameters=az_source)
+    extra = ExtraBody(dataSources=[az_data]) # type: ignore
+    req_settings = AzureChatRequestSettings(extra_body=extra) # type: ignore
+    prompt_config = sk.PromptTemplateConfig(execution_settings=req_settings)
+    chat_service = sk_oai.AzureChatCompletion(
+            deployment_name=deployment,
+            api_key=api_key,
+            endpoint=endpoint,
+            api_version="2023-12-01-preview",
+            use_extensions=True,
+        )
+    
+    #replace the existing chat service (that wasn't pointed at a specific Azure AI Search index) with the new chat service
+    kernel.remove_chat_service("chat-gpt")
+    kernel.add_chat_service("chat-gpt", chat_service)
+
+    prompt_template = OpenAIChatPromptTemplate(
+            template="{{$user_input}}", 
+            template_engine=kernel.prompt_template_engine, 
+            prompt_config=prompt_config)
+        
+    function_config = sk.SemanticFunctionConfig(prompt_config, prompt_template)        
+    chat_function = kernel.register_semantic_function("ChatBot", "Chat", function_config)
+    return chat_function
+
+async def chat(context: sk.KernelContext) -> bool:
+    try:
+        user_input = input("User:> ")
+        context.variables["user_input"] = user_input
+    except KeyboardInterrupt:
+        print("\n\nExiting chat...")
+        return False
+    except EOFError:
+        print("\n\nExiting chat...")
+        return False
+
+    if user_input == "exit":
+        print("\n\nExiting chat...")
+        return False
+    
+    #use the IntentDetectionPlugin-AssistantIntent plugin to detect the intent of the user input
+    context = await kernel.run(intent_plugin["AssistantIntent"], input_context=context)
+
+    #if the intent is found, then use the intent to setup the kernel for a specific index
+    if(context.result != "not_found"):
+        # get a new chat function that is pointed at a specific Azure AI Search index        
+        chat_function_for_index = setup_kernel_for_specific_index(context, kernel)
+
+        #answer the user's question using the specific index set up in the previous step
+        context = await kernel.run(chat_function_for_index, input_context=context)
+
+    context.variables["chat_history"] += f"\nUser:> {user_input}\nChatBot:> {context.result}\n"
+
+    print(f"ChatBot:> {context.result}")
+
+    print(f"Tool:> {context.objects.get('tool_message')}")
+ 
+    return True
+
+async def main() -> None:
+    context_vars = sk.ContextVariables()
+    context_vars["chat_history"] = ""
+    context = kernel.create_new_context(context_vars)
+    chatting = True
+    while chatting:
+        chatting = await chat(context)
+
+if __name__ == "__main__":
+    asyncio.run(main())
